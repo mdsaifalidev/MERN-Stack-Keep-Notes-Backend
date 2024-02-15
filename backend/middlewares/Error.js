7508e9fc7742c54eca9f1ee5df0a0a8353fed444
mdsaifalidev <159515806+mdsaifalidev@users.noreply.github.com>
2024-02-15 17:27:06 +0530

    Update Error.js

--- backend/middlewares/Error.js
+++ backend/middlewares/Error.js
@@ -4,7 +4,7 @@ const errorHandler = (err, req, res, next) => {
   if (err instanceof CustomError) {
     return res.status(err.status).json({ message: err.message });
   } else {
-    console.error(err.stack);
+    //console.error(err.stack);
     return res.status(500).json({ message: "Internal Server Error." });
   }
 };
